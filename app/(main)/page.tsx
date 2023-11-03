f83e52758dbb39790e1612d2f0e424af4adb5470
codeduchy <codeduchy@gmail.com>
2023-11-03 16:59:54 +0000

    auth and theme

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,5 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Spotify</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+      Discord
+      <ModeToggle />
+    </div>
+  );
 }
