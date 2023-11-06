d8cc0b567b49fcd00e6f5ea57fffe439b77892a3
codeduchy <codeduchy@gmail.com>
2023-11-06 16:40:33 +0000

    member management

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { CreateServerModal } from "../modals/create-server-modal";
 import { InviteModal } from "../modals/invite-modal";
 import { EditServerModal } from "../modals/edit-server-modal";
+import { MembersModal } from "../modals/members-modal";
 
 const ModalProvider = () => {
   // hydration error handling
@@ -19,6 +20,7 @@ const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal />
       <EditServerModal />
+      <MembersModal />
     </>
   );
 };
