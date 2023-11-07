103df24c5924e95cc70118a0278b6f547000a55c
codeduchy <codeduchy@gmail.com>
2023-11-07 17:21:16 +0000

    delete and edit channels

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -8,6 +8,8 @@ import { MembersModal } from "../modals/members-modal";
 import { CreateChannelModal } from "../modals/create-channel-modal";
 import { LeaveServerModal } from "../modals/leave-server-modal";
 import { DeleteServerModal } from "../modals/delete-server-modal";
+import { DeleteChannelModal } from "../modals/delete-channel-modal";
+import { EditChannelModal } from "../modals/edit-channel-modal copy";
 
 const ModalProvider = () => {
   // hydration error handling
@@ -27,6 +29,8 @@ const ModalProvider = () => {
       <CreateChannelModal />
       <LeaveServerModal />
       <DeleteServerModal />
+      <DeleteChannelModal />
+      <EditChannelModal />
     </>
   );
 };
